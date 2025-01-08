8ea5af93afdc4b104310df7d25281e0df34e26bb
Jurandi Junior <jurandijr20@gmail.com>
2025-01-08 19:44:38 -0300

    EXEMPLO 2 - VETORES

---
+++ Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace POO_CSharp_P6
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,43 @@
 ﻿using System.Runtime.ConstrainedExecution;
 using System.Globalization;
+using POO_CSharp_P6;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        //PROBLEMA EXEMPLO 2 - VETORES
+        //Fazer um programa para ler um número inteiro N e os dados(nome e
+        //preço) de N Produtos.Armazene os N produtos em um vetor.Em
+        //seguida, mostrar o preço médio dos produtos.
+
+        int n = int.Parse(Console.ReadLine());
+
+        Produto[] vect = new Produto[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string nome = Console.ReadLine();
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vect[i] = new Produto { Nome = nome, Preco = preco };
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            soma += vect[i].Preco;
+        }
+
+        double media = soma / n;
+
+        Console.WriteLine("Preço médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        //---------------------------------------------------------------------------------
         //PROBLEMA EXEMPLO 1 - VETORES
         //Fazer um programa para ler um número inteiro N e a altura de N pessoas.
         //Armazene as N alturas em um vetor. Em seguida, mostrar a altura média dessas
         //pessoas
-
+        /*
         int n = int.Parse(Console.ReadLine());
 
         double[] vect = new double[n];
@@ -29,6 +57,6 @@ internal class Program
         double media = soma / n;
 
         Console.WriteLine("Altura média = " + media.ToString("F2", CultureInfo.InvariantCulture));
-
+        */
     }
 }
